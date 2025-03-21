1a224c7d931cd9c55d415d0dc118bbe2f1dfdac4
xitikx <cheimonas31@gmail.com>
2025-03-21 22:24:23 +0530

    Made js file for all the components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import DashboardPafe from "./pages/DashboardPage";
+import Navbar from "./components/Navbar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<DashboardPafe />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
